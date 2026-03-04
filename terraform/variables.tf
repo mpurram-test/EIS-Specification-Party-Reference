de8008276f93863a80fd46d69d1879ab2d4c1a31
Mallareddy Purram <1Mpurra@seacoastbank.com>
2026-03-04 13:37:54 -0600

    updated variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,9 +1,5 @@
 variable "resource_group_name" { type = string }
 variable "api_management_name" { type = string }
+variable "spec_folder"        { type = string }
 variable "fail_if_no_specs"   { type = bool, default = true }
 variable "attach_api_policy"  { type = bool, default = true }
-variable "spec_folder" {
-  description = "Folder containing bundled OpenAPI spec files."
-  type        = string
-  default     = "build/api-bundled"
-}
